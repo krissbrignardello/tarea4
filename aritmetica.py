e79bac4229e8f07c59258d98b8194f87d9e14712
Kriss Brignardello <krissbrigna@gmail.com>
2020-09-06 23:48:48 -0300

    aritmetica.py

---
+++ aritmetica.py
@@ -0,0 +1,7 @@
+x=8
+y=5
+print (f'la suma es definida como {x}+{y}={x+y}')
+print (f'la resta se define como {x}-{y}={x-y}')
+print (f'la multiplicación se define como {x}*{y}={x*y}')
+print (f'la división se define como {x}/{y}={x/y}')
+print (f'el módulo es definido como el resto de la división, por lo tanto corresponde a {x}%{y}={x%y}')
