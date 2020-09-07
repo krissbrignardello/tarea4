d74206389b1ac47e14a640aa9648d1e6227b6605
Kriss Brignardello <krissbrigna@gmail.com>
2020-09-06 23:58:08 -0300

    diccionario.py

---
+++ diccionario.py
@@ -0,0 +1,8 @@
+diccionario = {'nombre': ['Vania','Polo','Marco','Moises'], 'apellido': ['Aguilera','Tello','Casas', 'Echeverría'],'cursos': ['Programacion', 'Fisica'], 'nota programacion': [63,21,56,68], 'nota fisica': [40,56,23,45]}
+print(diccionario['nombre'][0])
+print(diccionario['apellido'][0])
+print(diccionario['cursos'][0])
+print(diccionario['nota programacion'][0])
+print(diccionario['cursos'][1])
+print(diccionario['nota fisica'][0])
+
